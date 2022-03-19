1917ff00ccae9e0cdad5620650c89d3519f443a0
Vlad Badoiu <vlad_andrei.badoiu@upb.ro>
2022-03-19 11:57:18 +0200

    lab4: Update the skeleton to make IPv6 task optional

--- lab4/router.c
+++ lab4/router.c
@@ -1,10 +1,13 @@
 #include "skel.h"
 
+/* Array of router interfaces (e.g. 0,1,2,3) */
 int interfaces[ROUTER_NUM_INTERFACES];
 
+/* Routing table */
 struct rtable_entry *rtable;
 int rtable_len;
 
+/* Mac table */
 struct nei_entry *nei_table;
 int nei_table_len;
 
@@ -13,7 +16,7 @@ int nei_table_len;
  for the given protocol and destination address. Or NULL if there is no matching route.
 */
 struct rtable_entry *get_best_route(uint16_t proto, struct in_addr dest_ip, struct in6_addr dest_ip6) {
-	/* TODO 1: Implement the function */
+	/* TODO 1: Implement the function. We don't use dest_ip6 at this exercise */
 	return NULL;
 }
 
@@ -22,7 +25,7 @@ struct rtable_entry *get_best_route(uint16_t proto, struct in_addr dest_ip, stru
  for the given protocol and destination address. Or NULL if there is no matching route.
 */
 struct nei_entry *get_nei_entry(uint16_t proto, struct in_addr dest_ip, struct in6_addr dest_ip6) {
-    /* TODO 2: Implement the function */
+    /* TODO 2: Implement the function. We don't use dest_ip6 at this exercise. */
     return NULL;
 }
 
@@ -38,18 +41,21 @@ int main(int argc, char *argv[])
 
 	nei_table = malloc(sizeof(struct  nei_entry) * 100);
 	DIE(nei_table == NULL, "memory");
-
+	
+	/* Read the static routing table and the MAC table */
 	rtable_len = read_rtable(rtable);
 	nei_table_len = read_nei_table(nei_table);
-	/* Students will write code here */
 
 	while (1) {
+		/* Receives a packet from an interface */
 		rc = get_packet(&m);
 		DIE(rc < 0, "get_message");
-
+		
+		/* Extract the Ethernet header from the packet. Since protocols are stacked, the first header is the ethernet header,
+		 * the next header is at m.payload + sizeof(struct ether_header) */
 		struct ether_header *eth_hdr = (struct ether_header *) m.payload;
 
-		/* TODO 3: Check if this is an IPv4 or IPv6 packet and route accordingly. */
+		/* TODO 3: Check if this is an IPv4 or IPv6 packet and route accordingly. For now we will drop IPv6 packets and forward only IPv4.*/
 
 		/* TODO 4: Check the checksum as required by IPv4  */
 
